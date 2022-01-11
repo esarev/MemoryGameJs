31d0f626388847582b63f156348d91c05a096e92
Александр <esarev@inbox.ru>
2022-01-11 23:25:31 +0700

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const cardsArr = [
-    {imageName: 'shirt_card', imagePath: 'img/shirt_card.png'},
-    {imageName: 'shirt_card', imagePath: 'img/shirt_card.png'}
+    {imageName: 'image1', imagePath: 'img/shirt_card.png'},
+    {imageName: 'image2', imagePath: 'img/shirt_card.png'}
 ];
 
 cardsArr.sort(function() {return 0.5 - Math.random()});
@@ -9,3 +9,32 @@ console.log(cardsArr);
 const span = document.querySelector('#span'),
     cardsDiv = document.querySelector('.cardsDiv'),
     messageDiv = document.querySelector('.messageDiv');
+
+function createBoard() {
+    for (let i = 0; i < cardsArr; i++) {
+        const imgCard = document.createElement('IMG');
+        imgCard.setAttribute('id', String(i));
+        imgCard.setAttribute('src', 'img/shirt_card.png');
+        imgCard.addEventListener('click', flipCard);
+        cardsDiv.appendChild(imgCard);
+    }
+}
+
+createBoard();
+
+let cardsChosenArr =[];
+let cardsChosenArrId =[];
+let nOfOpenedCardsArr = [];
+
+function checkForMatch() {
+    const cards = document.querySelectorAll('.img');
+    const id1 = cardsChosenArrId[0];
+    const id2 = cardsChosenArrId[1];
+
+    if (cardsChosenArrId[0] === cardsChosenArrId[1] && id1 !== id2) {
+        messageDiv.innerHTML = 'Совпадение найдено!';
+        cards[id1].removeEventListener('click', flipCard);
+        cards[id2].removeEventListener('click', flipCard);
+        nOfOpenedCardsArr.push(cardsChosenArr);
+    }
+}
